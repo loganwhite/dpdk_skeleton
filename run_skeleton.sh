908297a256a10f3579104794614fd7ba59656bc1
Logan Von <loganwhitevon@gmail.com>
2026-02-03 21:26:15 +0800

    tuned for the best performance of the running parameter

--- run_skeleton.sh
+++ run_skeleton.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# ./build/skeleton -l 0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30 -n 4 --master-lcore=0 -w 4b:00.1,rxq_cqe_comp_en=1,mprq_en=1,rxqs_min_mprq=1,mprq_max_memcpy_len=128,txq_inline_max=64,txq_inline_mpw=64,mprq_log_stride_num=8 --socket-mem=8192,0
+./build/skeleton -l 0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30 -n 4 --master-lcore=0 -w 4b:00.1,rxq_cqe_comp_en=1,mprq_en=1,rxqs_min_mprq=1,mprq_max_memcpy_len=128,txq_inline_max=64,txq_inline_mpw=64,mprq_log_stride_num=8 --socket-mem=8192,0
 
 
 # ./build/skeleton -l 0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30 -n 4 --master-lcore=0 -w 4b:00.1,mprq_en=1,rxqs_min_mprq=1,mprq_max_memcpy_len=128,txq_inline_max=0,txq_inline_mpw=0,txq_mpw_en=0 --socket-mem=8192,0
 
-./build/skeleton -l 0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30 -n 4 --master-lcore=0 -w 4b:00.1,rxq_cqe_comp_en=1,txq_inline_max=0 --socket-mem=8192,0 --log-level=pmd.net.mlx5,debug
+# ./build/skeleton -l 0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30 -n 4 --master-lcore=0 -w 4b:00.1,rxq_cqe_comp_en=1,txq_inline_max=0 --socket-mem=8192,0 --log-level=pmd.net.mlx5,debug
